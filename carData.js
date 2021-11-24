529c329f99b1b9d6daa5b17b96a6ca3ffdc3fe80
Jared Campbell <jaredcampbell99@gmail.com>
2021-11-23 20:41:05 -0600

    created carData

---
+++ carData.js
@@ -0,0 +1,57 @@
+export const CAR_TYPES = {
+    SEDAN: 'Ethereum',
+    TRUCK: 'Bitcoin',
+    MOTORCYCLE: 'Motorcycle',
+    SUV: 'SUV'
+}
+
+export const CAR_DATA = [
+    {
+        id: 0,
+        name: 'Mazda 6',
+        year: '2007',
+        miles: 18675,
+        type: CAR_TYPES.SEDAN,
+        message: 'This is the first car'
+    },
+    {
+        id: 1,
+        name: 'Ford F150',
+        year: '2017',
+        miles: 98368,
+        type: CAR_TYPES.TRUCK,
+        message: 'This is the second car'
+    },
+    {
+        id: 2,
+        name: 'Chevy Suburban',
+        year: '2004',
+        miles: 3456,
+        type: CAR_TYPES.SUV,
+        message: 'This is the third car'
+    },
+    {
+        id: 3,
+        name: 'Toyota Camry',
+        year: '2013',
+        miles: 8761,
+        type: CAR_TYPES.SEDAN,
+        message: 'This is the 4th car'
+    },
+    {
+        id: 4,
+        name: 'Toyota Camry',
+        year: '2013',
+        miles: 8761,
+        type: CAR_TYPES.SEDAN,
+        message: 'This is the 4th car'
+    },
+    {
+        id: 5,
+        name: 'Toyota Camry',
+        year: '2013',
+        miles: 8761,
+        type: CAR_TYPES.SEDAN,
+        message: 'This is the 4th car'
+    }
+]
